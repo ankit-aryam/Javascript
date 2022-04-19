ad13263fe87652af0ddec13182c11730ef048fed
ankit.aryam <ankit.aryam01@gmail.com>
2022-04-19 15:14:07 +0530

    Github Scrapper complete

--- repoPage.js
+++ repoPage.js
@@ -1,5 +1,6 @@
 let request = require('request');
 let cheerio = require('cheerio');
+let getissues = require("./issues");
 
 function reposPage(url, topic){
 request(url,cb)
@@ -19,15 +20,20 @@ function getRepoLink(html){
    
    let headingArr = $('.f3.color-fg-muted.text-normal.lh-condensed');
   // console.log(headingArr);
-   console.log(topic);
-   console.log("-----------------------");
+  // console.log(topic);
+  // console.log("-----------------------");
    for(let i=0;i<8;i++){
         let topLink = $(headingArr[i]).find("a");
        // console.log(topLink);
-       let mainLink = $(topLink[0]).attr("href");
-        console.log(mainLink);
+       let mainLink = $(topLink[1]).attr("href");
+       // console.log(mainLink);
+       let repoName = mainLink.split('/').pop();
+       let fullLink = `https://github.com${mainLink}/issues`;
+       //console.log(fullLink);
+       
+       getissues(fullLink, topic, repoName)
    }
-   console.log("-----------------------");
+  // console.log("-----------------------");
 
 }
 
